31b99515d8c6c4395652555e02ffc46f7cfa77d8
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2024-03-29 14:25:44 +0000

    add mpre news

--- sql/insert.sql
+++ sql/insert.sql
@@ -2,4 +2,3 @@ INSERT INTO league (name, description) VALUES ('Olís deild karla', 'Efsta deild
 INSERT INTO league (name, description) VALUES ('Olís deild kvenna', 'Efsta deild kvenna í handbolta.');
 INSERT INTO league (name, description) VALUES ('Grill-66 deild karla', 'Næst efsta deild karla í handbolta.');
 INSERT INTO league (name, description) VALUES ('Grill-66 deild kvenna', 'Næst efsta og neðsta deild kvenna í handbolta.');
-INSERT INTO league (name, description) VALUES ('2. deild karla', 'Neðsta deild karla í handbolta.');
